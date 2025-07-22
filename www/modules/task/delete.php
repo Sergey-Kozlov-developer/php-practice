305a4c63e081bcb5885f9774c354ca39e83978f8
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-07-22 17:42:03 +0300

    task delete&&completed

--- www/modules/task/delete.php
+++ www/modules/task/delete.php
@@ -1,1 +1,14 @@
 <?php
+
+$pdo = DbConnect::db_connect();
+
+if (isset($_GET['id'])) {
+    $taskId = (int)$_GET['id'];
+
+    $stmt = $pdo->prepare("DELETE FROM tasks WHERE id = ?");
+
+    $stmt->execute([$taskId]);
+
+    header("Location: task");
+    exit();
+}
--- www/modules/task/toggle.php
+++ www/modules/task/toggle.php
@@ -1,1 +1,22 @@
 <?php
+
+$pdo = DbConnect::db_connect();
+
+if (isset($_GET['id'])) {
+	$taskId = (int)$_GET['id'];
+
+	$stmt = $pdo->prepare("SELECT is_completed FROM tasks WHERE id = ?");
+
+	$stmt->execute([$taskId]);
+
+    $task = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // меняем статус
+    $newStatus = !$task['is_completed'];
+
+    $stmt = $pdo->prepare("UPDATE tasks SET is_completed = ? WHERE id = ?");
+    $stmt->execute([$newStatus, $taskId]);
+
+    header("Location: task");
+    exit();
+}
